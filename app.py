800928703dcdd21ea4cb53bf75ef3f17bc984c90
elgazar24 <melgazar725@gmail.com>
2025-05-07 01:23:22 +0400

    G push Commit

--- app.py
+++ app.py
@@ -544,7 +544,7 @@ def create_app():
 
     @app.route('/favicon.ico')
     def favicon():
-        return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+        return send_from_directory( app.config['STATIC_FOLDER'], 'favicon.ico', mimetype='image/vnd.microsoft.icon')
   
     @app.errorhandler(404)
     def page_not_found(e):
